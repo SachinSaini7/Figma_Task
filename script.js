184856501076332e92fd3e29003590a5fae885a2
SachinSaini <sachinsaini.official30@gmail.com>
2025-11-30 22:26:41 +0530

    initial Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+let one=document.querySelector(".one img");
+let two=document.querySelector(".two img");
+
+setInterval(()=>{
+    one.src="./Assets/1.jpg";
+    two.src="./Assets/2.jpg";
+},7000)
+
+setInterval(()=>{
+    one.src="./Assets/3.jpg";
+    two.src="./Assets/4.jpg";
+},7000)
